a444d0e327f57c47155301709c62763eaa8773d9
arslnyhy <arsalan.jhnson@gmail.com>
2024-10-14 06:21:45 +0000

    Add RP rule

---
+++ netpicker/find_raspberry.py
@@ -0,0 +1,15 @@
+from comfy import high
+
+@high(
+    name='rule_find_raspberry'
+)
+
+def rule_find_raspberry(device):
+    host = 'https://sandbox.slurpit.io'
+    api_key = '4DTIz5axkWqOuxUGm6gobN4JAaB4l5Cg16589a08019c20'
+    api = slurpit.api(host, api_key)
+    snapshots = api.device.get_snapshots(hostname=device.ipaddress)
+    arp_results = snapshots['ARP']['planning_results']
+    vendors = {arp['Mac_vendor'] for arp in arp_results}
+    assert 'Raspberry Pi Foundation' not in vendors, "ALERT: SUSPICIOUS DEVICE DETECTED IN THE NETWORK!"LS
+    
