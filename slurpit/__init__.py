6674e1f10d30986c5234a0475042bba0b826f567
arslnyhy <arsalan.jhnson@gmail.com>
2024-10-14 06:32:45 +0000

    cmt

--- slurpit/__init__.py
+++ slurpit/__init__.py
@@ -6,6 +6,7 @@ import re
 # Create the table for VLANs if not exists
 create_table('cisco_ios')
 
+
 def handle_command(command: str):
     if command.startswith("vlan"):
         # Get the VLAN number
@@ -22,9 +23,11 @@ def handle_command(command: str):
             # If VLAN exists, update it; otherwise, create a new record
             if existing_vlans:
                 if vlan_name not in existing_vlans:
-                    update_record("cisco_ios", "vlan", existing_vlans + f"\n{vlan_number} {vlan_name} {vlan_status} {vlan_ports}")
+                    update_record("cisco_ios", "vlan", existing_vlans +
+                                  f"\n{vlan_number} {vlan_name} {vlan_status} {vlan_ports}")
             else:
-                create_record("cisco_ios", "vlan", f"{vlan_number} {vlan_name} {vlan_status} {vlan_ports}")
+                create_record("cisco_ios", "vlan", f"{vlan_number} {
+                              vlan_name} {vlan_status} {vlan_ports}")
             return True
 
     if command == "show vlan brief":
@@ -32,18 +35,19 @@ def handle_command(command: str):
         vlans = read_record("cisco_ios", "vlan")
         if vlans:
             # Format the VLANs into a table
-            table_header = "{:<4} {:<32} {:<9} {:<30}".format("VLAN", "Name", "Status", "Ports")
+            table_header = "{:<4} {:<32} {:<9} {:<30}".format(
+                "VLAN", "Name", "Status", "Ports")
             table_separator = "-" * len(table_header)
             table_rows = [table_header, table_separator]
 
             # Add each VLAN to the table
             for vlan in vlans.split("\n"):
                 vlan_details = vlan.split(" ")
-                vlan_row = "{:<4} {:<32} {:<9} {:<30}".format(vlan_details[0], vlan_details[1], vlan_details[2], vlan_details[3])
+                vlan_row = "{:<4} {:<32} {:<9} {:<30}".format(
+                    vlan_details[0], vlan_details[1], vlan_details[2], vlan_details[3])
                 table_rows.append(vlan_row)
-            
+
             # Join the rows into a formatted string
             return "\n".join(table_rows)\
 
     return ""
-
